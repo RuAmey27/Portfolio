6349b4a0e396c61084f4f1ce12178d5ccfdb1f14
RuAmey27 <ameypande2709@gmail.com>
2024-08-03 14:53:23 +0530

    Intial commit before making the build

---
+++ src/content_option.js
@@ -0,0 +1,201 @@
+const logotext = "AMEY";
+const meta = {
+  title: "Amey Pande",
+  description:
+    "I’m Amey Pande Full stack devloper,currently persuing the BTech in Shri Guru Gobind SinghJi Institute Of Engineering & Technology ",
+};
+
+const introdata = {
+  title: "I’m Amey Pande",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I am passionate about developing both frontend and backend",
+  },
+  description:
+    "As a full-stack development fresher, I'm passionate about learning new technologies and building innovative solutions. I'm always open to new opportunities and challenges.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "A bit about my self",
+  aboutme:
+    "I'm Amey Pande, a full-stack development fresher with a strong foundation in various technologies, including C, MySQL, NextJS, OOP concepts, Data Structures, Java, Algorithms, Git, GitHub, Blockchain, AWS & cloud services, Docker, Machine Learning, and Python. My passion for coding and technology drives me to constantly learn and implement new concepts. I have worked on several projects demonstrating my skills, ranging from command line tools to full-stack web applications.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Secratory",
+        where: "E-Cell SGGSIE&T",
+        date: "08/2023 – 08/2024",
+      },
+      {
+        jobtitle: "Event Manager",
+        where: "PRAGYAA2K24",
+        date: "01/2024 – 04/2024",
+      },
+      {
+        jobtitle: "Assistant Web Developer",
+        where: "PRAGYAA2K23",
+        date: "11/2022 – 03/2023",
+      },
+];
+
+const skills = [
+
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+{
+    name: "C",
+    value: 90,
+  },
+  {
+    name: "Java",
+    value: 85,
+  },
+  {
+    name: "MySQL",
+    value: 80,
+  },
+  {
+    name: "NextJS",
+    value: 75,
+  },
+  {
+    name: "Python",
+    value: 80,
+  },
+  {
+    name: "AWS & Cloud Services",
+    value: 70,
+  },
+];
+
+const services = [
+    {
+        title: "Full-Stack Development",
+        description:
+          "I develop responsive and robust full-stack applications using technologies like Java, C, MySQL, and NextJS.",
+      },
+      {
+        title: "Machine Learning",
+        description:
+          "I implement machine learning models to solve real-world problems, using Python and associated libraries.",
+      },
+      {
+        title: "Cloud Services",
+        description:
+          "I have experience working with AWS & Cloud Services, enabling scalable and reliable solutions.",
+      },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "Mock Test Application, command line tool. This is a basic mock test application focusing on file handling and basic C concepts. It randomly selects questions from a dataset and records results to a text file with the student's name.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "LIFI Based Data Transfer System, A microcontroller Based System. This project showcases data transfer using LED light and LDR sensor. An Arduino-based receiver and transmitter interchange textual data using Arduino C.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "Sign Language Animator, A Django Based Application. This project converts text or audio input to sign language animation, aiming to help the deaf community.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "GPA Calculator For College. Developed a GPA Calculator for an autonomous college, enabling dynamic updates of subjects and credits. Implemented in Spring Boot with a backend system for real-time updates and a user-friendly interface.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "LIFI Based Data Transfer System, A microcontroller Based System. This project showcases data transfer using LED light and LDR sensor. An Arduino-based receiver and transmitter interchange textual data using Arduino C.",
+      
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "LIFI Based Data Transfer System, A microcontroller Based System. This project showcases data transfer using LED light and LDR sensor. An Arduino-based receiver and transmitter interchange textual data using Arduino C.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "Sign Language Animator, A Django Based Application. This project converts text or audio input to sign language animation, aiming to help the deaf community.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "GPA Calculator For College. Developed a GPA Calculator for an autonomous college, enabling dynamic updates of subjects and credits. Implemented in Spring Boot with a backend system for real-time updates and a user-friendly interface.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "Sign Language Animator, A Django Based Application. This project converts text or audio input to sign language animation, aiming to help the deaf community.",
+      
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "ameypande2709@gmail.com",
+  YOUR_FONE: "+91 7773927352",
+  description:
+    "As a full-stack development fresher, I'm passionate about learning new technologies and building innovative solutions. I'm always open to new opportunities and challenges.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/RuAmey27",
+  // facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/ameypande/",
+  //twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
